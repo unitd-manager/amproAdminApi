b872b0ccbb69d4334747246a3125bc94c2f8354a
syed jasmine <smsaf91@gmail.com>
2025-11-17 11:17:02 +0530

    first commit

--- routes/address.js
+++ routes/address.js
@@ -18,7 +18,7 @@ app.use(
     createParentPath: true,
   })
 );
-app.get('/getTrack', (req, res, next) => {
+app.get('/getTrack1234', (req, res, next) => {
     db.query(`SELECT e.*
         ,en.first_name AS shipment
     FROM customer_address e
